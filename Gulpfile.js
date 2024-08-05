651e17012cc9875223b10842982733ba392a8938
Denise Grassi <denygrassi_@hotmail.com>
2024-08-05 15:53:18 -0300

    public

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,29 +3,33 @@ const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
+// Tarefa para compilar estilos SCSS para CSS e salvar em "public/css"
 function styles() {
-    return gulp.src('./src/styles/*.scss')   /* caminho p pegar tods os arquivos da pasta styles dentro de src com .scss*/
+    return gulp.src('./src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(gulp.dest('./dist/css')); 
-};
-
-
-function images() {
-    return gulp.src('./src/images/**/*') 			/*    ** p acessar as pags   */
-    .pipe(imagemin())
-    .pipe(gulp.dest('./dist/images')); 
+        .pipe(gulp.dest('./public/css')); 
 }
 
+// Tarefa para otimizar imagens e salvar em "public/images"
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./public/images')); 
+}
+
+// Tarefa para minificar scripts JavaScript e salvar em "public/js"
 function scripts() {
-    return gulp.src('./src/scripts/*.js')   
+    return gulp.src('./src/scripts/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('./dist/js')); 
-};
+        .pipe(gulp.dest('./public/js')); 
+}
 
-exports.default = gulp.parallel(styles, images, scripts); 			/* executar o styles e o imagemin juntos */
+// Tarefa padrão para executar todas as tarefas em paralelo
+exports.default = gulp.parallel(styles, images, scripts);
 
+// Tarefa para assistir a mudanças em arquivos SCSS e JS
 exports.watch = function () {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
-    gulp.watch('./src/scripts/*.js',gulp.parallel(scripts))
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
 };
 
